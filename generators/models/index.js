75e31a6ed036991103fc71fa71b35b6489dc3246
Wesley <wesley.nmtorres@gmail.com>
2017-05-16 09:55:37 -0300

    change router strategy

--- generators/models/index.js
+++ generators/models/index.js
@@ -1,76 +1,80 @@
 // jshint esversion: 6
 (function() {
-    'use strict';
+  'use strict';
 
-    const Generator = require('yeoman-generator');
-    const chalk = require('chalk');
-    const yosay = require('yosay');
-    const path = require('path');
-    const _ = require('lodash');
-    const pluralize = require('pluralize');
-    const dasherize = require('dasherize');
+  const Generator = require('yeoman-generator');
+  const chalk = require('chalk');
+  const yosay = require('yosay');
+  const path = require('path');
+  const _ = require('lodash');
+  const pluralize = require('pluralize');
+  const dasherize = require('dasherize');
 
-    var copyTemplate = (fs, template, path, options) => {
-      fs.copyTpl(template, path, options);
-    };
+  var copyTemplate = (fs, template, path, options) => {
+    fs.copyTpl(template, path, options);
+  };
 
-    module.exports = class extends Generator {
+  module.exports = class extends Generator {
 
-      constructor(args, opts) {
-        super(args, opts);
+    constructor(args, opts) {
+      super(args, opts);
 
-        this.argument('model', {
-          type: String,
-          desc: 'In path format. Ex: `./Account.json`',
-          required: true
-        });
+      this.argument('model', {
+        type: String,
+        desc: 'In path format. Ex: `./Account.json`',
+        required: true
+      });
 
-        this.options.modelObj = this.fs.readJSON(this.options.model);
+      this.options.modelObj = this.fs.readJSON(this.options.model);
 
-        this.options.appname = path.basename(process.cwd());
-        this.props = {};
-      }
+      this.options.appname = path.basename(process.cwd());
+      this.props = {};
+    }
 
-      initializing() {
-        this.log(yosay(
-          'Welcome to the awesome ' + chalk.red('generator-ingenico-api:models') + ' generator!'
-        ));
-      }
+    initializing() {
+      this.log(yosay(
+        'Welcome to the awesome ' + chalk.red('generator-ingenico-api:models') + ' generator!'
+      ));
+    }
 
-      prompting() {
-        this.questions = [];
-        return this.prompt(this.questions).then((answers) => {
-          this.props = answers;
-        });
-      }
+    prompting() {
+      this.questions = [];
+      return this.prompt(this.questions).then((answers) => {
+        this.props = answers;
+      });
+    }
 
-      configuring() {
-        this.destinationRoot('./server');
-      }
+    configuring() {
+      this.destinationRoot('./server');
+    }
 
-      writing() {
-        copyTemplate(this.fs, this.templatePath('_model.js'), this.destinationPath('models/' + this.options.modelObj.name + '.model.js'), {
-          modelName: this.options.modelObj.name,
-          modelObj: this.options.modelObj,
-          modelNamelc: this.options.modelObj.name.toLowerCase()
-        });
-        copyTemplate(this.fs, this.templatePath('_crud-model.js'), this.destinationPath('controllers/' + this.options.modelObj.name + '-crud-model.js'), {
-          modelName: this.options.modelObj.name
-        });
-        copyTemplate(this.fs, this.templatePath('routes/_routes.js'), this.destinationPath('routes/' + this.options.modelObj.name + '.js'), {
-          modelName: this.options.modelObj.name
-        });
-        copyTemplate(this.fs, this.templatePath('_migration.js'), this.destinationPath('../migrations/' + Date.now() + '-create-' + pluralize.plural(dasherize(this.options.modelObj.name)) + '.js'), {
-            modelName: dasherize(this.options.modelObj.name),
-            modelNamelc: this.options.modelObj.name.toLowerCase(),
-            modelObj: this.options.modelObj
-          });
+    writing() {
+      copyTemplate(this.fs, this.templatePath('_model.js'), this.destinationPath('models/' + this.options.modelObj.name + '.model.js'), {
+        modelName: this.options.modelObj.name,
+        modelObj: this.options.modelObj,
+        modelNamelc: this.options.modelObj.name.toLowerCase()
+      });
+      copyTemplate(this.fs, this.templatePath('_crud-model.js'), this.destinationPath('controllers/' + this.options.modelObj.name + '-crud-model.js'), {
+        modelName: this.options.modelObj.name
+      });
+      copyTemplate(this.fs, this.templatePath('routes/_routes.js'), this.destinationPath('routes/' + this.options.modelObj.name + '.js'), {
+        modelName: this.options.modelObj.name
+      });
+      copyTemplate(this.fs, this.templatePath('_migration.js'), this.destinationPath('../migrations/' + Date.now() + '-create-' + pluralize.plural(dasherize(this.options.modelObj.name)) + '.js'), {
+        modelName: dasherize(this.options.modelObj.name),
+        modelNamelc: this.options.modelObj.name.toLowerCase(),
+        modelObj: this.options.modelObj
+      });
 
-          var importValue = "import " + _.camelCase(this.options.modelObj.name) + " from './server/routes/" + this.options.modelObj.name + "';";
-          var useValue = _.camelCase(this.options.modelObj.name) + "(app);";
-          var file = this.fs.read(this.destinationPath('../app.js')); file = file.replace('//import-inject', importValue + '\n//import-inject'); file = file.replace('//router-inject', useValue + '\n//router-inject'); this.fs.write(this.destinationPath('../app.js'), file);
-        }
+      var modelObjCamel = _.camelCase(this.options.modelObj.name);
+      var importValue = "import " + modelObjCamel + " from './server/routes/" + this.options.modelObj.name + "';";
+      var useValue = "app.use('/" + modelObjCamel + "/'," + modelObjCamel + "(app));";
+      var file = this.fs.read(this.destinationPath('../app.js'));
+      file = file.replace('//import-inject', importValue + '\n//import-inject');
+      file = file.replace('//router-inject', useValue + '\n//router-inject');
+      this.fs.write(this.destinationPath('../app.js'), file);
+    }
 
-        end() {}
-      };
-    })();
+    end() {}
+  };
+})();
--- generators/models/templates/routes/_routes.js
+++ generators/models/templates/routes/_routes.js
@@ -1,46 +1,50 @@
 //jshint esversion:6
-import <%= modelName %>Controller from '../controllers/<%= modelName %>-crud-model';
+import <%= modelName %>
+Controller from '../controllers/<%= modelName %>-crud-model';
+import express from 'express';
+let router = express.Router();
 
 export default (app) => {
   const <%- modelName.toLowerCase() %>Controller = new <%= modelName %>Controller(app.dbconfig.models.<%= modelName %>);
-  app.route('/<%- modelName.toLowerCase() %>')
-  // .all(app.auth.authenticate())
-  .get((req, res) => {
+
+  router.get('/<%- modelName.toLowerCase() %>', (req, res) => {
     <%- modelName.toLowerCase() %>Controller.getAll()
-    .then(response => {
-      res.status(response.statusCode);
-      res.json(response.data);
-    });
-  })
-  .post((req, res) => {
-    <%- modelName.toLowerCase() %>Controller.create(req.body)
-    .then(response => {
-      res.status(response.statusCode);
-      res.json(response.data);
-    });
+      .then(response => {
+        res.status(response.statusCode);
+        res.json(response.data);
+      });
   });
 
-  app.route('/<%- modelName.toLowerCase() %>/:id')
-  // .all(app.auth.authenticate())
-  .get((req, res) => {
-    <%- modelName.toLowerCase() %>Controller.getById(req.params)
-    .then(response => {
-      res.status(response.statusCode);
-      res.json(response.data);
-    });
-  })
-  .put((req, res) => {
-    <%- modelName.toLowerCase() %>Controller.update(req.body, req.params)
-    .then(response => {
-      res.status(response.statusCode);
-      res.json(response.data);
-    });
-  })
-  .delete((req, res) => {
-    <%- modelName.toLowerCase() %>Controller.delete(req.params)
-    .then(response => {
-      res.status(response.statusCode);
-      res.json(response.data);
-    });
+  router.post('/<%- modelName.toLowerCase() %>', (req, res) => {
+    <%- modelName.toLowerCase() %>Controller.create(req.body)
+      .then(response => {
+        res.status(response.statusCode);
+        res.json(response.data);
+      });
   });
+
+  router.get('/<%- modelName.toLowerCase() %>/:id', (req, res) => {
+    <%- modelName.toLowerCase() %>Controller.getById(req.params)
+      .then(response => {
+        res.status(response.statusCode);
+        res.json(response.data);
+      });
+  });
+
+  router.put('/<%- modelName.toLowerCase() %>/:id', (req, res) => {
+    <%- modelName.toLowerCase() %>Controller.update(req.body, req.params)
+      .then(response => {
+        res.status(response.statusCode);
+        res.json(response.data);
+      });
+  });
+
+  router.delete('/<%- modelName.toLowerCase() %>/:id', (req, res) => {
+    <%- modelName.toLowerCase() %>Controller.delete(req.params)
+      .then(response => {
+        res.status(response.statusCode);
+        res.json(response.data);
+      });
+  });
+  return router;
 };
